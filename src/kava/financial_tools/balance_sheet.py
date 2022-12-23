def049c7717efcc6d8f4650b2108a4d77c8d20f8
yuma300 <ywakimoto2s@gmail.com>
2022-12-23 14:58:57 +0900

    add financial tools

---
+++ src/kava/financial_tools/balance_sheet.py
@@ -0,0 +1,50 @@
+from decimal import Decimal
+import logging
+
+logger = logging.getLogger(name=__name__)
+logger.addHandler(logging.NullHandler())
+
+
+def set_root_logger():
+  root_logget = logging.getLogger(name=None)
+  root_logget.setLevel(logging.INFO)
+  stream_handler = logging.StreamHandler()
+  stream_handler.setLevel(logging.INFO)
+  root_logget.addHandler(stream_handler)
+
+class BalanceSeet:
+    def __init__(self):
+        self.balance_sheet = {}
+
+    def put_spot_balance_sheet(self, currency: str, amount: Decimal):
+      self.balance_sheet.setdefault(currency, {"spot": Decimal("0.0"), "lend": Decimal("0.0"), "borrow": Decimal("0.0")})
+      if self.balance_sheet[currency]["spot"] + amount < 0:
+        logger.error(f'amount become less than 0. amount: {amount} currency {currency}')
+      self.balance_sheet[currency]["spot"] += amount
+
+    def put_lend_balance_sheet(self, currency: str, amount: Decimal) -> tuple: # (回収額, 利息)のタプルを返す
+      self.balance_sheet.setdefault(currency, {"spot": Decimal("0.0"), "lend": Decimal("0.0"), "borrow": Decimal("0.0")})
+      result = (abs(amount), 0)
+      if self.balance_sheet[currency]["lend"] + amount < 0:
+        interest = abs(self.balance_sheet[currency]["lend"] + amount)
+        result = (self.balance_sheet[currency]["lend"], interest)
+        self.balance_sheet[currency]["lend"] = 0
+        self.balance_sheet[currency]["spot"] += interest
+      else:
+        self.balance_sheet[currency]["lend"] += amount
+      return result
+
+    def put_borrow_balance_sheet(self, currency: str, amount: Decimal) -> tuple: # (返済額, 利息)のタプルを返す
+      self.balance_sheet.setdefault(currency, {"spot": Decimal("0.0"), "lend": Decimal("0.0"), "borrow": Decimal("0.0")})
+      result = (abs(amount), 0)
+      if self.balance_sheet[currency]["borrow"] + amount < 0:
+        interest = abs(self.balance_sheet[currency]["borrow"] + amount)
+        result = (self.balance_sheet[currency]["borrow"], interest)
+        self.balance_sheet[currency]["borrow"] = 0
+        self.balance_sheet[currency]["spot"] -= interest
+      else:
+        self.balance_sheet[currency]["borrow"] += amount
+      return result
+
+    def get_balance_sheet_dict(self) -> dict:
+        return self.balance_sheet
